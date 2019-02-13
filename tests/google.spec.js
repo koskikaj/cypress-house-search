1811d4c052062757e6638fb19a595a3c547d8a4e
koskikaj <43949263+koskikaj@users.noreply.github.com>
2019-02-13 14:56:28 +0200

    Create google.spec.js

---
+++ tests/google.spec.js
@@ -0,0 +1,14 @@
+describe('My First Test', function() {
+  it('Search Digia on Google', function() {
+  //Selain näkymän koko
+  cy.viewport(2440, 1800)
+  //Sivu jota testataan  
+  cy.visit('http://www.google.com/')
+  //Kirjoita google hakuun Digia	
+	cy.get('input[name="q"]').type('Digia')
+  //Klikataan google-haku nappia
+	cy.contains('Google-haku').click()
+  //Assert
+	cy.contains('Osoite:').next().should('contain', 'Atomitie')
+  })
+})
