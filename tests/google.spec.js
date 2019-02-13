d3640e0ce9778ee333e108cc6be488984e72e244
koskikaj <43949263+koskikaj@users.noreply.github.com>
2019-02-13 15:18:47 +0200

    Update google.spec.js

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -5,12 +5,12 @@
   //Selain näkymän koko
   cy.viewport(2440, 1800)
   //Sivu jota testataan  
-    cy.visit('http://www.google.com/')
+  cy.visit('http://www.google.com/')
   //Kirjoita google hakuun Digia	
-	cy.get('input[name="q"]').type('Digia Helsinki')
+  cy.get('input[name="q"]').type('Digia Helsinki')
   //Klikataan google-haku nappia
-	cy.contains('Google-haku').click()
+  cy.contains('Google-haku').click()
   //Assert
-	cy.contains('Osoite:').next().should('contain', 'Atomitie')
+  cy.contains('Osoite:').next().should('contain', 'Atomitie')
   })
 })
