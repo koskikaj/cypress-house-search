ac888068aa5e522edd8ef64141d31b6ab75da018
koskikaj <43949263+koskikaj@users.noreply.github.com>
2019-02-13 15:18:22 +0200

    Update google.spec.js

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -1,11 +1,13 @@
-describe('My First Test', function() {
+  //Varmista Digian Helsingin konttorin osoite
+
+  describe('My First Test', function() {
   it('Search Digia on Google', function() {
   //Selain näkymän koko
   cy.viewport(2440, 1800)
   //Sivu jota testataan  
-  cy.visit('http://www.google.com/')
+    cy.visit('http://www.google.com/')
   //Kirjoita google hakuun Digia	
-	cy.get('input[name="q"]').type('Digia')
+	cy.get('input[name="q"]').type('Digia Helsinki')
   //Klikataan google-haku nappia
 	cy.contains('Google-haku').click()
   //Assert
